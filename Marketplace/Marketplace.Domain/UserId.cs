3a557412007cf7a61b3b6d65e7e0aaab82086395
Pete Maroun <pmaroun@microsoft.com>
2023-01-17 08:26:51 -0600

    intial source commit

---
+++ Marketplace/Marketplace.Domain/UserId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public record UserId (Guid Value);
+
+}
---
+++ Marketplace/Marketplace.Tests/Money.cs
@@ -0,0 +1,51 @@
+using Marketplace.Domain;
+
+namespace Marketplace.Tests
+{
+    public class MoneyTests
+    {
+        [Fact]
+        public void Money_objects_with_the_same_amount_should_be_equal()
+        {
+
+            var firstAmount = new Money(123);
+            var secondAmount = new Money(123);
+
+            Assert.Equal(firstAmount, secondAmount);
+
+        }
+        [Fact]
+        public void Money_objects_can_be_added()
+        {
+
+            var firstAmount = new Money(123);
+            var secondAmount = new Money(123);
+
+            var sum = firstAmount.Add(secondAmount);
+            Assert.Equal(sum.Amount, 246);
+
+        }
+        [Fact]
+        public void Money_objects_can_be_subtracted()
+        {
+
+            var firstAmount = new Money(123);
+            var secondAmount = new Money(123);
+
+            var sum = firstAmount.Substract(secondAmount);
+            Assert.Equal(sum.Amount, 0);
+
+        }
+        [Fact]
+        public void Money_objects_can_be_subtracted_with_operation()
+        {
+
+            var firstAmount = new Money(123);
+            var secondAmount = new Money(123);
+
+            var sum = firstAmount - secondAmount;
+            Assert.Equal(sum.Amount, 0);
+
+        }
+    }
+}
